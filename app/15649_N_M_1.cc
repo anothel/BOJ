4cce01be3e5c5ca7f861dd1a1cd00fa82e8a4eee
PilkyuJung <anothel1@naver.com>
2022-01-30 10:17:18 +0900

    15649_N_M_1

---
+++ app/15649_N_M_1.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+enum : int { MAX_NUM = 8 };
+
+static int N(0), M(0);
+static std::vector<int> vAnswer;
+static bool bVisited[MAX_NUM];
+
+void backTracking(const int &count) {
+  if (count == M) {
+    for (int i = 0; i < M; i++) std::cout << vAnswer[i] << " ";
+    std::cout << "\n";
+
+    return;
+  }
+
+  for (int i = 0; i < N; i++) {
+    if (bVisited[i] == false) {
+      bVisited[i] = true;
+      vAnswer.push_back(i + 1);
+      backTracking(count + 1);
+      vAnswer.pop_back();
+      bVisited[i] = false;
+    }
+  }
+
+  return;
+}
+
+int main() {
+  std::cout.tie(NULL);
+  std::cin.tie(NULL);
+  std::ios_base::sync_with_stdio(false);
+
+  std::cin >> N >> M;
+
+  backTracking(0);
+
+  return 0;
+}
