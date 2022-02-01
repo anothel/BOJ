c1d3d33ad9911e3e8e9b5cd4c40abeea93e7abf8
PilkyuJung <anothel1@naver.com>
2022-02-01 12:08:32 +0900

    15651_N_M_3

---
+++ app/15651_N_M_3.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+enum : int { MAX_NUM = 8 };
+
+static int N(0), M(0);
+static std::vector<int> vAnswer;
+static bool bVisited[MAX_NUM];
+
+void dfs(const int& count) {
+    if (M == count) {
+        for (int i = 0; i < M; i++) std::cout << vAnswer[i] << " ";
+        std::cout << "\n";
+        return;
+    }
+
+    for (int i = 0; i < N; i++) {
+        vAnswer.push_back(i + 1);
+        dfs(count + 1);
+        vAnswer.pop_back();
+    }
+    return;
+}
+
+int main() {
+    std::cout.tie(NULL);
+    std::cin.tie(NULL);
+    std::ios_base::sync_with_stdio(false);
+
+    std::cin >> N >> M;
+
+    dfs(0);
+
+    return 0;
+}
