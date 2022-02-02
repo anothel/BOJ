1f45f378bbb3b0f9fd0f0772c7b2eb3c7d731c74
PilkyuJung <anothel1@naver.com>
2022-02-02 10:38:57 +0900

    15654_N_M_5

---
+++ app/15654_N_M_5.cc
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+enum : int { MAX_NUM = 8 };
+
+static int N(0), M(0), Input(0);
+static std::vector<int> vAnswer;
+static std::vector<int> vInput;
+static bool bVisited[MAX_NUM];
+
+void dfs(const int& count) {
+  if (M == count) {
+    for (int i = 0; i < M; i++) std::cout << vAnswer[i] << " ";
+    std::cout << "\n";
+
+    return;
+  }
+
+  for (int i = 0; i < N; i++) {
+    if (bVisited[i] == false) {
+      bVisited[i] = true;
+      vAnswer.push_back(vInput.at(i));
+      dfs(count + 1);
+      vAnswer.pop_back();
+      bVisited[i] = false;
+    }
+  }
+
+  return;
+}
+
+int main() {
+  std::cout.tie(NULL);
+  std::cin.tie(NULL);
+  std::ios_base::sync_with_stdio(false);
+  std::cin >> N >> M;
+
+  for (int i = 0; i < N; i++) {
+    std::cin >> Input;
+    vInput.push_back(Input);
+  }
+  sort(vInput.begin(), vInput.end());
+
+  dfs(0);
+
+  return 0;
+}
