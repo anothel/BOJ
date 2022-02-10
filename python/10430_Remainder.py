650abb63e5bdd3c212389b4835af7c510a05ef45
PilkyuJung <anothel1@naver.com>
2022-02-10 12:32:58 +0900

    10430_Remainder

---
+++ python/10430_Remainder.py
@@ -0,0 +1,11 @@
+def main():
+  A, B, C = map(int, input().split())
+
+  print((A+B) % C)
+  print(((A % C) + (B % C)) % C)
+  print((A*B) % C)
+  print(((A % C) * (B % C)) % C)
+
+
+if __name__ == "__main__":
+    main()
