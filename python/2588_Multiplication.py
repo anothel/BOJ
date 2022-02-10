8b15e17bc848d8b7ba0ada86af19aecbae219859
PilkyuJung <anothel1@naver.com>
2022-02-10 12:32:30 +0900

    2588_Multiplication

--- python/2588_Multiplication.py
+++ python/2588_Multiplication.py
@@ -1,11 +1,17 @@
 import sys
 
-data = [sys.stdin.readline().strip() for i in range(2)]
 
-x1 = int(data[0])
-x2 = int(data[1])
+def main():
+  data = [sys.stdin.readline().strip() for i in range(2)]
 
-print(x1*(x2 % 10))
-print(x1*int((x2/10) % 10))
-print(x1*int(x2/100))
-print(x1*x2)
+  x1 = int(data[0])
+  x2 = int(data[1])
+
+  print(x1*(x2 % 10))
+  print(x1*int((x2/10) % 10))
+  print(x1*int(x2/100))
+  print(x1*x2)
+
+
+if __name__ == "__main__":
+    main()
