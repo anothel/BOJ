8a6a7ce42142edfde042b4f8ee7217654f868d24
Pilkyu <anothel1@naver.com>
2022-02-19 10:59:16 +0900

    2480_ThreeDice

---
+++ python/2480_ThreeDice.py
@@ -0,0 +1,25 @@
+from sys import stdin
+
+
+def main():
+  price = 0
+  A, B, C = map(int, stdin.readline().split())
+  if A == B == C:
+    price = 10000 + A * 1000
+  elif A == B or A == C:
+    price = 1000 + A * 100
+  elif B == C:
+    price = 1000 + B * 100
+  else:
+    dice_list = list()
+    dice_list.insert(0, A)
+    dice_list.insert(0, B)
+    dice_list.insert(0, C)
+    dice_list.sort()
+    price = dice_list[2] * 100
+
+  print(price)
+
+
+if __name__ == "__main__":
+    main()
