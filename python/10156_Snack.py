128e83ba302e688449dcb3db3b7a4eef58367de8
Pilkyu <anothel1@naver.com>
2022-02-19 11:12:52 +0900

    10156_Snack

--- python/10156_Snack.py
+++ python/10156_Snack.py
@@ -3,7 +3,11 @@ from sys import stdin
 
 def main():
   K, N, M = map(int, stdin.readline().split())
-  print(K * N - M)
+  needMoney = K * N - M
+  if needMoney < 0:
+    needMoney = 0
+
+  print(needMoney)
 
 
 if __name__ == "__main__":
