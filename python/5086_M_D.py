270a122486f05c45de617f26ff72b40bf90bbaba
Pilkyu <anothel1@naver.com>
2022-02-20 09:00:14 +0900

    5086_M_D

---
+++ python/5086_M_D.py
@@ -0,0 +1,20 @@
+from posixpath import split
+from sys import stdin
+
+
+def main():
+  while 1:
+    A, B = map(int, stdin.readline().split())
+    if A == 0 and B == 0:
+      break
+
+    if B % A == 0:
+      print("factor")
+    elif A % B == 0:
+      print("multiple")
+    else:
+      print("neither")
+
+
+if __name__ == "__main__":
+    main()
