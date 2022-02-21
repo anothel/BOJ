057eed9937498b6a4f7f7ff022e2b92099380c52
Pilkyu <anothel1@naver.com>
2022-02-21 18:11:54 +0900

    2577_CountOfNumber

---
+++ python/2577_CountOfNumber.py
@@ -0,0 +1,20 @@
+from sys import stdin
+
+
+def main():
+  r = 1
+  numList = [0 for _ in range(10)]
+
+  for _ in range(3):
+    r *= int(stdin.readline().strip())
+
+  while 0 < r:
+    numList[r % 10] += 1
+    r //= 10
+
+  for i in range(10):
+    print(numList[i])
+
+
+if __name__ == "__main__":
+    main()
