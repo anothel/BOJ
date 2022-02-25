918877031a2c01730337aae548dab2bcfc885bf4
Pilkyu <anothel1@naver.com>
2022-02-25 10:13:22 +0900

    4948_BertrandsPostulate

--- python/4948_BertrandsPostulate.py
+++ python/4948_BertrandsPostulate.py
@@ -12,21 +12,23 @@ def prime_list(n: int) -> list:
   return [i for i in range(2, n) if primeList[i] == True]
 
 
-def bertran(n: int) -> int:
+def bertran(n: int, l: list) -> int:
   count: int = 0
-  for i in prime_list((2 * n) + 1):
+  for i in l:
     if n < i and i <= 2 * n:
       count += 1
   return count
 
 
 def main():
+  primeList: list = prime_list(123456 * 2 + 1)
+  
   while True:
     n = int(stdin.readline().strip())
     if n == 0:
       break
 
-    print(bertran(n))
+    print(bertran(n, primeList))
 
 
 if __name__ == "__main__":
