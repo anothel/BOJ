662b65a074ed39a2bd92e25605288e9b83f01bcf
Pilkyu <anothel1@naver.com>
2022-03-09 16:10:40 +0900

    5338_MicrosoftLogo

---
+++ app/5338_MicrosoftLogo.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+  std::cout.tie(NULL);
+  std::cin.tie(NULL);
+  std::ios_base::sync_with_stdio(false);
+
+  std::cout << "       _.-;;-._ "
+            << "\n";
+  std::cout << "'-..-'|   ||   |"
+            << "\n";
+  std::cout << "'-..-'|_.-;;-._|"
+            << "\n";
+  std::cout << "'-..-'|   ||   |"
+            << "\n";
+  std::cout << "'-..-'|_.-''-._|"
+            << "\n";
+
+  return 0;
+}
