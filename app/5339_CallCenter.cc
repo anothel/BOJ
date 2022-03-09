e326a6e84b50233ed1cdfcbde813d5e73d1aa952
Pilkyu <anothel1@naver.com>
2022-03-09 16:14:33 +0900

    5339_CallCenter

---
+++ app/5339_CallCenter.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main() {
+  std::cout.tie(NULL);
+  std::cin.tie(NULL);
+  std::ios_base::sync_with_stdio(false);
+
+  std::cout << "     /~\\"
+            << "\n";
+  std::cout << "    ( oo|"
+            << "\n";
+  std::cout << "    _\\=/_"
+            << "\n";
+  std::cout << "   /  _  \\"
+            << "\n";
+  std::cout << "  //|/.\\|\\\\"
+            << "\n";
+  std::cout << " ||  \\ /  ||"
+            << "\n";
+  std::cout << "============"
+            << "\n";
+  std::cout << "|          |"
+            << "\n";
+  std::cout << "|          |"
+            << "\n";
+  std::cout << "|          |"
+            << "\n";
+
+  return 0;
+}
