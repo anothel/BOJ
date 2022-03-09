cd72e215d041bd93f7693c8e5b830fea7409fb13
Pilkyu <anothel1@naver.com>
2022-03-09 16:07:55 +0900

    5337_Welcome

---
+++ app/5337_Welcome.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+  std::cout.tie(NULL);
+  std::cin.tie(NULL);
+  std::ios_base::sync_with_stdio(false);
+
+  std::cout << ".  .   ." << "\n";
+  std::cout << "|  | _ | _. _ ._ _  _" << "\n";
+  std::cout << "|/\\|(/.|(_.(_)[ | )(/." << "\n";
+
+  return 0;
+}
