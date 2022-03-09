bedd698c3323051219a59becb9d4c845142b5c0b
Pilkyu <anothel1@naver.com>
2022-03-09 17:05:03 +0900

    9653_StarWarsLogo

---
+++ app/9653_StarWarsLogo.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+  std::cout.tie(NULL);
+  std::cin.tie(NULL);
+  std::ios_base::sync_with_stdio(false);
+
+  std::cout << "    8888888888  888    88888\n";
+  std::cout << "   88     88   88 88   88  88\n";
+  std::cout << "    8888  88  88   88  88888\n";
+  std::cout << "       88 88 888888888 88   88\n";
+  std::cout << "88888888  88 88     88 88    888888\n";
+  std::cout << "\n";
+  std::cout << "88  88  88   888    88888    888888\n";
+  std::cout << "88  88  88  88 88   88  88  88\n";
+  std::cout << "88 8888 88 88   88  88888    8888\n";
+  std::cout << " 888  888 888888888 88  88      88\n";
+  std::cout << "  88  88  88     88 88   88888888\n";
+
+  return 0;
+}
